15f8cc731b93aa6f4ec03b145f55a22c08fc911a
jalhagosipo <gpfla757@naver.com>
2022-06-18 15:21:59 +0900

    refactor: 순서 의존성 리팩토링

--- chapter6/src/main/java/Game.java
+++ chapter6/src/main/java/Game.java
@@ -12,7 +12,9 @@ public class Game {
         int ball = 0;
         int score = 0;
         for (int currentFrame = 0; currentFrame < theFrame; currentFrame++) {
-            score += itsThrows[ball++] + itsThrows[ball++];
+            int firstThrow = itsThrows[ball++];
+            int secondThrow = itsThrows[ball++];
+            score += firstThrow + secondThrow;
         }
         return score;
     }
