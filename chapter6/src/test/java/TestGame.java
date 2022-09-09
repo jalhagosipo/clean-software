a3bdd05ca7e8115f6885a3c1ae37032b419eec50
jalhagosipo <gpfla757@naver.com>
2022-09-09 18:35:08 +0900

    feat: 점수넣기 테스트 - Bowling

--- chapter6/src/test/java/TestGame.java
+++ chapter6/src/test/java/TestGame.java
@@ -71,4 +71,63 @@ public class TestGame {
         assertEquals(300, g.score());
         assertEquals(11, g.getCurrentFrame());
     }
+
+    @Test
+    public void testEndOfArray()
+    {
+        for (int i=0; i<9; i++)
+        {
+            g.add(0);
+            g.add(0);
+        }
+        g.add(2);
+        g.add(8); // 10번째 프레임의 스페어
+        g.add(10); // 배열 마지막 위치에서의 스트라이크
+        assertEquals(20, g.score());
+    }
+
+    @Test
+    public void testSampleGame()
+    {
+        g.add(1);
+        g.add(4);
+        g.add(4);
+        g.add(5);
+        g.add(6);
+        g.add(4);
+        g.add(5);
+        g.add(5);
+        g.add(10);
+        g.add(0);
+        g.add(1);
+        g.add(7);
+        g.add(3);
+        g.add(6);
+        g.add(4);
+        g.add(10);
+        g.add(2);
+        g.add(8);
+        g.add(6);
+        assertEquals(133, g.score());
+    }
+
+    @Test
+    public void testHeartBreak()
+    {
+        for (int i=0; i<11; i++)
+            g.add(10);
+        g.add(9);
+        assertEquals(299, g.score());
+    }
+
+    @Test
+    public void testTenthFrameSpare()
+    {
+        for (int i=0; i<9; i++)
+            g.add(10);
+        g.add(9);
+        g.add(1);
+        g.add(1);
+        assertEquals(270, g.score());
+    }
 }
